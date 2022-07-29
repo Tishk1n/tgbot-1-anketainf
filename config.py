5ca2c07f491b97fb6300d00c90cb995e77c8e8d7
Rodion <timofeevyar@gmail.com>
2022-07-29 19:41:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 TOKEN = "5312485815:AAF1Net5p941x2z1hnjTGfV2ddjwSEVnojo"
 ADMIN= 1678937897
+=======
+TOKEN = "5517641650:AAH6G5xoRqOe5J7QV9jHvWaEr4R-N_csxUE"
+ADMIN= 1678937897
+>>>>>>> Stashed changes
