0942f232712830e92d53612a681d2cd55c3023bb
Rodion <timofeevyar@gmail.com>
2022-07-29 17:51:13 +0500

    Залил бота на гит!

---
+++ markups.py
@@ -0,0 +1,255 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+
+
+
+
+
+inline_btn_start = InlineKeyboardButton('Перейти к вопросам', callback_data='start', )
+
+inline_btn_ndfl = InlineKeyboardButton("2-НДФЛ", callback_data='ndfl')
+inline_btn_pfr = InlineKeyboardButton("Выписка из ПФР", callback_data='pfr')
+inline_btn_nd = InlineKeyboardButton("Налоговая декларация", callback_data='nd')
+inline_btn_srp = InlineKeyboardButton("Справка о размере пенсии", callback_data='srp')
+inline_btn_spfb = InlineKeyboardButton("Справка по форме банка", callback_data='spfb')
+inline_btn_vph = InlineKeyboardButton("Выписка из похозяйственной книги", callback_data='vph')
+inline_btn_notpay = InlineKeyboardButton("Без подтверждения", callback_data='notpay')
+
+inline_btn_cob = InlineKeyboardButton("Собственность", callback_data='cob')
+inline_btn_coz = InlineKeyboardButton("Социальный найм", callback_data='coz')
+inline_btn_are = InlineKeyboardButton("Аренда", callback_data='are')
+inline_btn_voi = InlineKeyboardButton("Воинская часть", callback_data='voi')
+inline_btn_jr = InlineKeyboardButton("Жильё родственников", callback_data='jr')
+inline_btn_com = InlineKeyboardButton("Коммунальная квартира", callback_data='com')
+
+inline_btn_uc = InlineKeyboardButton("Ученая степень", callback_data='uc')
+inline_btn_2v = InlineKeyboardButton("Два высших и более", callback_data='2v')
+inline_btn_vuc = InlineKeyboardButton("Высшее", callback_data='vuc')
+inline_btn_nv = InlineKeyboardButton("Неоконченное высшее", callback_data='nv')
+inline_btn_cz = InlineKeyboardButton("Среднее специальное", callback_data='cz')
+inline_btn_cr = InlineKeyboardButton("Среднее", callback_data='cr')
+inline_btn_nuvc = InlineKeyboardButton("Ниже среднего", callback_data='nuvc')
+inline_btn_rm = InlineKeyboardButton("Российское МВА", callback_data='rm')
+inline_btn_umba = InlineKeyboardButton("Иностранное МВА", callback_data='umba')
+
+
+inline_btn_jz = InlineKeyboardButton("Женат/замужем", callback_data='jz')
+inline_btn_gr = InlineKeyboardButton("Гражданский брак", callback_data='gr')
+inline_btn_hz = InlineKeyboardButton("Холост/не замужем", callback_data='hz')
+inline_btn_raz = InlineKeyboardButton("Разведен(-а)", callback_data='raz')
+inline_btn_vdo = InlineKeyboardButton("Вдовец/вдова", callback_data='vdo')
+
+
+inline_btn_yes = InlineKeyboardButton("Есть", callback_data='yes')
+inline_btn_no = InlineKeyboardButton("Нет", callback_data='no')
+inline_btn_bzc = InlineKeyboardButton("Будет заключен до сделки", callback_data='bzc')
+
+inline_btn_work = InlineKeyboardButton("Работает", callback_data='work')
+inline_btn_nowork = InlineKeyboardButton("Не работает", callback_data='nowork')
+inline_btn_pen = InlineKeyboardButton("На пенсии", callback_data='pen')
+
+
+inline_btn_kom = InlineKeyboardButton("Коммерческая", callback_data='kom')
+inline_btn_byd = InlineKeyboardButton("Бюджетная", callback_data='byd')
+inline_btn_cvoy_buz = InlineKeyboardButton("Свой бизнес", callback_data='cdoy_buz')
+inline_btn_naym = InlineKeyboardButton("По найму", callback_data='naym')
+inline_btn_pencioner = InlineKeyboardButton("Пенсионер", callback_data='pencioner')
+inline_btn_ip = InlineKeyboardButton("ИП", callback_data='ip')
+
+
+inline_btn_down = InlineKeyboardButton("Загрузить карточку", callback_data='down')
+inline_btn_vvesti = InlineKeyboardButton("Ввести вручную", callback_data='vvesti')
+
+inline_btn_fin = InlineKeyboardButton("Посмотреть анкету", callback_data='fin')
+
+
+
+
+ndfl = InlineKeyboardButton("2-НДФЛ", callback_data='ndfl1')
+pfr = InlineKeyboardButton("Выписка из ПФР", callback_data='pfr1')
+nd = InlineKeyboardButton("Налоговая декларация", callback_data='nd1')
+srp = InlineKeyboardButton("Справка о размере пенсии", callback_data='srp1')
+spfb = InlineKeyboardButton("Справка по форме банка", callback_data='spfb1')
+vph = InlineKeyboardButton("Выписка из похозяйственной книги", callback_data='vph1')
+notpay = InlineKeyboardButton("Без подтверждения", callback_data='notpay1')
+
+pray_d = InlineKeyboardMarkup().row(ndfl)\
+    .row(pfr)\
+    .row(nd)\
+    .row(srp)\
+    .row(spfb)\
+    .row(vph)\
+    .row(notpay)\
+
+cob = InlineKeyboardButton("Собственность", callback_data='cob1')
+coz = InlineKeyboardButton("Социальный найм", callback_data='coz1')
+are = InlineKeyboardButton("Аренда", callback_data='are1')
+voi = InlineKeyboardButton("Воинская часть", callback_data='voi1')
+jr = InlineKeyboardButton("Жильё родственников", callback_data='jr1')
+com = InlineKeyboardButton("Коммунальная квартира", callback_data='com1')
+
+osnva = InlineKeyboardMarkup().row(cob)\
+    .row(coz)\
+    .row(are)\
+    .row(voi)\
+    .row(jr)\
+    .row(com)\
+
+
+
+
+uc = InlineKeyboardButton("Ученая степень", callback_data='uc1')
+_2v = InlineKeyboardButton("Два высших и более", callback_data='2v1')
+vuc = InlineKeyboardButton("Высшее", callback_data='vuc1')
+nv = InlineKeyboardButton("Неоконченное высшее", callback_data='nv1')
+cz = InlineKeyboardButton("Среднее специальное", callback_data='cz1')
+cr = InlineKeyboardButton("Среднее", callback_data='cr1')
+nuvc = InlineKeyboardButton("Ниже среднего", callback_data='nuvc1')
+rm = InlineKeyboardButton("Российское МВА", callback_data='rm1')
+umba = InlineKeyboardButton("Иностранное МВА", callback_data='umba1')
+
+educ_c = InlineKeyboardMarkup().row(uc)\
+    .row(_2v)\
+    .row(vuc)\
+    .row(nv)\
+    .row(cz)\
+    .row(cr)\
+    .row(nuvc)\
+    .row(rm)\
+    .row(umba)\
+
+
+jz = InlineKeyboardButton("Женат/замужем", callback_data='jz1')
+gr = InlineKeyboardButton("Гражданский брак", callback_data='gr1')
+hz = InlineKeyboardButton("Холост/не замужем", callback_data='hz1')
+raz = InlineKeyboardButton("Разведен(-а)", callback_data='raz1')
+vdo = InlineKeyboardButton("Вдовец/вдова", callback_data='vdo1')
+
+fam1 = InlineKeyboardMarkup().row(jz)\
+    .row(gr)\
+    .row(hz)\
+    .row(raz)\
+    .row(vdo)\
+
+
+yes = InlineKeyboardButton("Есть", callback_data='yes1')
+no = InlineKeyboardButton("Нет", callback_data='no1')
+bzc = InlineKeyboardButton("Будет заключен до сделки", callback_data='bzc1')
+
+bra1 = InlineKeyboardMarkup().row(yes)\
+    .row(no)\
+    .row(bzc)
+
+work = InlineKeyboardButton("Работает", callback_data='work1')
+nowork = InlineKeyboardButton("Не работает", callback_data='nowork1')
+pen = InlineKeyboardButton("На пенсии", callback_data='pen1')
+
+cosual_c1 = InlineKeyboardMarkup().row(work)\
+    .row(nowork)\
+    .row(pen)
+
+
+kom = InlineKeyboardButton("Коммерческая", callback_data='kom1')
+byd = InlineKeyboardButton("Бюджетная", callback_data='byd1')
+cvoy_buz = InlineKeyboardButton("Свой бизнес", callback_data='cdoy_buz1')
+naym = InlineKeyboardButton("По найму", callback_data='naym1')
+pencioner = InlineKeyboardButton("Пенсионер", callback_data='pencioner1')
+ip = InlineKeyboardButton("ИП", callback_data='ip1')
+
+zan1 = InlineKeyboardMarkup().row(kom)\
+    .row(byd)\
+    .row(cvoy_buz)\
+    .row(naym)\
+    .row(pencioner)\
+    .row(ip)\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fin = InlineKeyboardMarkup().row(inline_btn_fin)
+
+
+card_m = InlineKeyboardMarkup().row(inline_btn_down).row(inline_btn_vvesti)
+
+
+zan = InlineKeyboardMarkup().row(inline_btn_kom)\
+    .row(inline_btn_byd)\
+    .row(inline_btn_cvoy_buz)\
+    .row(inline_btn_naym)\
+    .row(inline_btn_pencioner)\
+    .row(inline_btn_ip)\
+
+cosual_c = InlineKeyboardMarkup().row(inline_btn_work)\
+    .row(inline_btn_nowork)\
+    .row(inline_btn_pen)
+
+
+
+
+
+inline_btn_stop = InlineKeyboardButton("ЗАКОНЧИЛ", callback_data='stop')
+
+inline_btn_cor = InlineKeyboardButton("Исправить анкету", callback_data='cor')
+inline_btn_cor_c = InlineKeyboardButton("Исправить карточку компании", callback_data='cor_c')
+
+corective = InlineKeyboardMarkup().row(inline_btn_cor)
+corective_c = InlineKeyboardMarkup().row(inline_btn_cor, inline_btn_cor_c)
+
+start = InlineKeyboardMarkup().row(inline_btn_start)
+
+
+pay_d = InlineKeyboardMarkup().row(inline_btn_ndfl)\
+    .row(inline_btn_pfr)\
+    .row(inline_btn_nd)\
+    .row(inline_btn_srp)\
+    .row(inline_btn_spfb)\
+    .row(inline_btn_vph)\
+    .row(inline_btn_notpay)\
+
+osnv = InlineKeyboardMarkup().row(inline_btn_cob)\
+    .row(inline_btn_coz)\
+    .row(inline_btn_are)\
+    .row(inline_btn_voi)\
+    .row(inline_btn_jr)\
+    .row(inline_btn_com)\
+
+educ = InlineKeyboardMarkup().row(inline_btn_uc)\
+    .row(inline_btn_2v)\
+    .row(inline_btn_vuc)\
+    .row(inline_btn_nv)\
+    .row(inline_btn_cz)\
+    .row(inline_btn_cr)\
+    .row(inline_btn_nuvc)\
+    .row(inline_btn_rm)\
+    .row(inline_btn_umba)\
+
+fam = InlineKeyboardMarkup().row(inline_btn_jz)\
+    .row(inline_btn_gr)\
+    .row(inline_btn_hz)\
+    .row(inline_btn_raz)\
+    .row(inline_btn_vdo)\
+
+bra = InlineKeyboardMarkup().row(inline_btn_yes)\
+    .row(inline_btn_no)\
+    .row(inline_btn_bzc)
+
+
+
+
+
+
+continue_menu = InlineKeyboardMarkup().row(inline_btn_stop)
+
---
+++ sqlite_db.py
@@ -0,0 +1,318 @@
+import sqlite3
+import time
+class Database:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.cursor = self.connection.cursor()
+
+    #_____________________________Добавление юзера_________________________--
+    def examination(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,)).fetchall()
+            return bool(len(res))
+
+    def add(self, user_id):
+        with self.connection:
+            return self.connection.execute("INSERT INTO users ('user_id') VALUES (?)", (user_id,))
+
+
+    # _____________________________Активация анкеты_________________________--
+
+    def user_anketa(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT anketa FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return int(res[0][0])
+
+    def add_anketa(self, user_id, anketa):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET anketa = ? WHERE user_id = ?", (anketa, user_id))
+
+    def user_pay_d(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT pay_d FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_pay_d(self, user_id, pay_d):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET pay_d = ? WHERE user_id = ?", (pay_d, user_id))
+
+    def user_adres(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT adres FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_adres(self, user_id, adres):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET adres = ? WHERE user_id = ?", (adres, user_id))
+
+    def user_time_live(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT time_live FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_time_live(self, user_id, time_live):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET time_live = ? WHERE user_id = ?", (time_live, user_id))
+
+    def user_osnovanie(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT osnovanie FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_osnovanie(self, user_id, osnovanie):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET osnovanie = ? WHERE user_id = ?", (osnovanie, user_id))
+
+    def user_education(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT education FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_education(self, user_id, education):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET education = ? WHERE user_id = ?", (education, user_id))
+
+    def user_family(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT family FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_family(self, user_id, family):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET family= ? WHERE user_id = ?", (family, user_id))
+
+    def user_brak(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT brak FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_brak(self, user_id, brak):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET brak= ? WHERE user_id = ?", (brak, user_id))
+
+
+    def user_cosual(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT cosual FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_cosual(self, user_id, cosual):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET cosual= ? WHERE user_id = ?", (cosual, user_id))
+
+    def user_e_mail(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT e_mail FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_e_mail(self, user_id, e_mail):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET e_mail= ? WHERE user_id = ?", (e_mail, user_id))
+
+    def user_phone_number(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT phone_number FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_phone_number(self, user_id, phone_number):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET phone_number= ? WHERE user_id = ?", (phone_number, user_id))
+
+    def user_cnulc(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT cnulc FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_cnulc(self, user_id, cnulc):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET cnulc= ? WHERE user_id = ?", (cnulc, user_id))
+
+    def user_zan(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT zan FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_zan(self, user_id, zan):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET zan= ? WHERE user_id = ?", (zan, user_id))
+
+    def user_card(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT card FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_card(self, user_id, card):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET card= ? WHERE user_id = ?", (card, user_id))
+
+
+
+    #_____________________________COMPANY___________________________
+
+    def examination_com(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT * FROM down_card WHERE user_id = ?", (user_id,)).fetchall()
+            return bool(len(res))
+
+    def add_com(self, user_id):
+        with self.connection:
+            return self.connection.execute("INSERT INTO down_card ('user_id') VALUES (?)", (user_id,))
+
+    def user_name(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT name FROM down_card WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_name(self, user_id, name):
+        with self.connection:
+            return self.cursor.execute("UPDATE down_card SET name= ? WHERE user_id = ?", (name, user_id))
+
+    def user_adres_company(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT adres_company FROM down_card WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_adres_company(self, user_id, adres_company):
+        with self.connection:
+            return self.cursor.execute("UPDATE down_card SET adres_company= ? WHERE user_id = ?", (adres_company, user_id))
+
+    def user_years(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT years FROM down_card WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_years(self, user_id, years):
+        with self.connection:
+            return self.cursor.execute("UPDATE down_card SET years= ? WHERE user_id = ?", (years, user_id))
+
+    def user_sait(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT saits FROM down_card WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_saits(self, user_id, sait):
+        with self.connection:
+            return self.cursor.execute("UPDATE down_card SET saits= ? WHERE user_id = ?", (sait, user_id))
+
+    def user_number_com(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT number_com FROM down_card WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_number_com(self, user_id, number_com):
+        with self.connection:
+            return self.cursor.execute("UPDATE down_card SET number_com= ? WHERE user_id = ?", (number_com, user_id))
+
+    def user_count(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT count FROM down_card WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_count(self, user_id, count):
+        with self.connection:
+            return self.cursor.execute("UPDATE down_card SET count= ? WHERE user_id = ?", (count, user_id))
+
+    def user_cfera(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT cfera FROM down_card WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_cfera(self, user_id, cfera):
+        with self.connection:
+            return self.cursor.execute("UPDATE down_card SET cfera= ? WHERE user_id = ?", (cfera, user_id))
+
+    #______________________________________________________________________________________
+
+
+    def user_start_work(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT start_work FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_start_work(self, user_id, start_work):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET start_work= ? WHERE user_id = ?", (start_work, user_id))
+
+    def user_start_work2(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT start_work2 FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_start_work2(self, user_id, start_work2):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET start_work2= ? WHERE user_id = ?", (start_work2, user_id))
+
+    def user_dolg(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT dolg FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_dolg(self, user_id, dolg):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET dolg= ? WHERE user_id = ?", (dolg, user_id))
+
+    def user_dohod(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT dohod FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_dohod(self, user_id, dohod):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET dohod= ? WHERE user_id = ?", (dohod, user_id))
+
+    def user_dohod2(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT dohod2 FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_dohod2(self, user_id, dohod2):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET dohod2= ? WHERE user_id = ?", (dohod2, user_id))
+
+    def user_credit(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT credit FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_credit(self, user_id, credit):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET credit= ? WHERE user_id = ?", (credit, user_id))
+
+    def user_credit_card(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT credit_card FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_credit_card(self, user_id, credit_card):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET credit_card= ? WHERE user_id = ?", (credit_card, user_id))
+
+    def user_limi(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT limi FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_limi(self, user_id, limi):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET limi= ? WHERE user_id = ?", (limi, user_id))
+
+    def user_car(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT car FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_car(self, user_id, car):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET car= ? WHERE user_id = ?", (car, user_id))
+
+    def user_cob(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT cob FROM users WHERE user_id = ?", (user_id,)).fetchmany(1)
+            return res[0][0]
+
+    def add_cob(self, user_id, cob):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET cob= ? WHERE user_id = ?", (cob, user_id))
+
+
+
